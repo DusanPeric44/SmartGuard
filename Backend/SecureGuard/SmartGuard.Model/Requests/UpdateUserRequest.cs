d7f0913de70604206258b837c97b6fc9b2460aba
Dušan Perić <dukemajstor@gmail.com>
2026-05-29 22:17:34 +0200

    backend: update user, firstName and lastName not required

--- Backend/SecureGuard/SmartGuard.Model/Requests/UpdateUserRequest.cs
+++ Backend/SecureGuard/SmartGuard.Model/Requests/UpdateUserRequest.cs
@@ -4,11 +4,8 @@ namespace SmartGuard.Model.Requests
 {
     public class UpdateUserRequest
     {
-        [Required]
-        public string FirstName { get; set; } = string.Empty;
-
-        [Required]
-        public string LastName { get; set; } = string.Empty;
+        public string? FirstName { get; set; } = string.Empty;
+        public string? LastName { get; set; } = string.Empty;
 
         [Required]
         public string Role { get; set; } = string.Empty;
