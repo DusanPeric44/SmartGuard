96e58405cf5ad33bfdc9f16342d295dfd672e830
marko-milidragovic <marko.milidragovic@gmail.com>
2026-06-03 22:27:45 +0200

    fps changed

--- Backend/SecureGuard/SmartGuard.Archive.Microservice/Controllers/UploadController.cs
+++ Backend/SecureGuard/SmartGuard.Archive.Microservice/Controllers/UploadController.cs
@@ -171,7 +171,7 @@ namespace SmartGuard.Archive.Microservice.Controllers
             }
 
             var conversion = FFmpeg.Conversions.New();
-            conversion.AddParameter($"-r 3 -i \"{inputPath}\" -c:v libx264 -pix_fmt yuv420p \"{outputPath}\"");
+            conversion.AddParameter($"-r 10 -i \"{inputPath}\" -c:v libx264 -pix_fmt yuv420p \"{outputPath}\"");
             await conversion.Start(cancellationToken);
         }
 
